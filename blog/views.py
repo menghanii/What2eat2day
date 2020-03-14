71dafdcfc7d7024c3cf9dc6f167e6593f6f40160
ymh <suneverset@naver.com>
2020-03-15 01:24:36 +0900

    recent posts go up, carousel with recent posts

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.utils import timezone
 from .models import Post, Comment
 
 def main_page(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/main_page.html', {'posts':posts})
+    posts = Post.objects.all().order_by('-created_date')
+    last_post = Post.objects.order_by('created_date').last()
+    return render(request, 'blog/main_page.html', {
+        'posts':posts,
+        'last_post':last_post,
+        })
 
 
 def post_detail(request, post_id):
