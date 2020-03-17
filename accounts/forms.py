5b4a83452d1460e7fe2fe77c028e29e758fdca32
ymh <suneverset@naver.com>
2020-03-17 17:15:03 +0900

    login/register page center align

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
-    real_name = forms.CharField(max_length = 20, min_length= 2)
+    # real_name = forms.CharField(max_length = 20, min_length= 2)
     class Meta:
         model = User
         fields = ('username', 'real_name')
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'size': 12, 'class': 'form-control', 'style': 'width: 30%', 'placeholder': '아이디를 적어주세요~'}
+            ),
+            'real_name': forms.TextInput(
+                attrs={'size': 12, 'class': 'form-control', 'style': 'width: 30%', 'placeholder': '이름을 적어주세요~'},
+            ),
+        }
   
 
 class CustomAuthenticationForm(AuthenticationForm):
--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,35 @@
 from django import forms
 from .models import Post, Comment
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+ 
 
 class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'bulletin', 'menu', 'image', 'image_2', 'image_3', 'image_4', 'text',)
 
+        widgets = {
+            'title': forms.TextInput(
+                attrs={'size': 12, 'class': 'form-control', 'style': 'width: 70%', 'placeholder': '어디에 다녀오셨나요~?'}
+            ),
+            'bulletin': forms.Select(
+                attrs={'class': 'custom-select', 'style': 'width: 50%',},
+            ),
+            'menu': forms.Select(
+                attrs={'class': 'custom-select', 'style': 'width: 50%',},
+            ),
+            'text':  forms.Textarea(
+                attrs={'size': 10,'class': 'form-control', 'style': 'width: 70%', 'placeholder': '후기를 작성해주세요! 정성껏 쓸 수록 진심이 더해집니다~'}
+            ),
+        }
+
 
 class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        widgets = {
+            'content': forms.TextInput(
+                attrs={'size': 12, 'class': 'form-control', 'style': 'width: 70%', 'placeholder': '한 줄 평을 남겨주세요~'}
+            ),
+        }
