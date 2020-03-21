c89f8601f7649ce9a294979b5f189531c719e4b4
Lee Kyunghwan <khrock1@gmail.com>
2020-03-21 16:27:28 +0900

    data

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,11 +9,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = config('DEBUG')
+
+SECRET_KEY='ns=4@5d9spkd4s3edw+o!%x=k_u^o9mj9w*6scef8av2d21b5#'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
